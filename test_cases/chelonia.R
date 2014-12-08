eaa4e4cfbba781c41dc3760cd18b39966fb45062
pbastide <paul.bastide@gmail.com>
2014-12-08 09:44:16 +0100

    add OUwie fit to chelonia

--- test_cases/chelonia.R
+++ test_cases/chelonia.R
@@ -31,7 +31,9 @@ source("R/shifts_manipulations.R")
 source("R/plot_functions.R")
 source("R/parsimonyNumber.R")
 
-load("../data/chelonia.rda")
+## Import data set form package geiger
+library(geiger)
+data(chelonia)
 
 tree <- chelonia$phy
 data <- chelonia$dat
@@ -39,7 +41,9 @@ data <- data[match(tree$tip.label, names(data))]
 
 plot(tree, show.tip.label = FALSE)
 
-# EM
+###############################################################################
+## EM
+##############################################################################
 K <- 16
 time <- system.time(
   results_estim_EM <- estimateEM(phylo = tree, 
@@ -71,6 +75,7 @@ time <- system.time(
                                                           "lasso"))
 )
 
+## Save History and process
 name <- paste0("_K=", K)
 history <- list_to_table.history(results_estim_EM$params_history)
 write.csv2(history, paste0(PATH, "boite_noire", name, ".csv"))
@@ -83,4 +88,86 @@ plot.process.actual(Y.state = data,
                     Z.state = results_estim_EM$ReconstructedNodesStates,
                     phylo = tree, 
                     paramsEstimate = results_estim_EM$params)
+
+## Equivalent Solutions
+times_shared <- compute_times_ca(tree)
+t_tree <-  min(node.depth.edgelength(tree)[1:ntaxa])
+T_tree <- incidence.matrix(tree)
+ac_tree <- incidence_matrix_actualization_factors(tree = tree, 
+                                                  selection.strength = results_estim_EM$params$selection.strength,
+                                                  times_shared = times_shared)
+T_tree_ac <- T_tree * ac_tree
+
+eq_shifts_edges_sim <- equivalent_shifts_edges(tree, results_estim_EM$params$shifts$edges)
+eq_shifts_values_sim <- equivalent_shifts_values(tree,
+                                                 shifts = results_estim_EM$params$shifts,
+                                                 beta_0 = results_estim_EM$params$optimal.value,
+                                                 eq_shifts_edges = eq_shifts_edges_sim,
+                                                 T_tree_ac = T_tree_ac)
+
+plot_equivalent_shifts.actual(tree, eq_shifts_edges_sim, eq_shifts_values_sim, use.edge.length = FALSE, adj = 0)
+
+## Save Image
 save.image(paste0(PATH, "estimation", name, ".RData"))
+rm(results_estim_EM, history, times_shared, t_tree, T_tree, ac_tree, T_tree_ac, eq_shifts_edges_sim, eq_shifts_values_sim)
+
+###############################################################################
+## OUwie
+###############################################################################
+
+## Import Data from Jaffe
+vv <- read.csv("../data/Chelonia_habitats_vector.txt", header = FALSE)
+chel_df <- NULL
+chel_df$species <- as.character(vv[c(2:44, 178:222, 362:406, 546:590, 730:774, 914:916), ])
+chel_df$accession_number <-  as.character(vv[c(46:88, 224:268, 408:452, 592:636, 776:820, 918:920), ])
+tmp <- vv[c(90:132, 270:314, 454:498, 638:682, 822:866, 922:924), ]
+chel_df$length <- as.numeric(levels(tmp))[tmp]
+chel_df$habitat <-  factor(vv[c(134:176, 316:360, 500:544, 684:728, 868:912, 926:928), ])
+chel_df <- as.data.frame(chel_df)
+# Correspondance
+chel_df <- chel_df[match(names(data), chel_df$species), ]
+
+## Clustering Habitat
+clusters <- chel_df$habitat
+Nbr_ParSols <- extract.parsimonyNumber(parsimonyNumber(tree, clusters))
+ParSols <- extract.enumerate_parsimony(enumerate_parsimony(tree, clusters))
+Nbr_shifts_OUwie <- extract.parsimonyCost(parsimonyCost(tree, clusters), 227)
+
+## format data for OUwie
+treeWIE <- tree
+treeWIE$node.label <- ParSols[1,(length(tree$tip.label)+1):dim(ParSols)[2]]
+dataWIE <- chel_df[ ,c("species", "habitat", "length")]
+dataWIE$length <- log(dataWIE$length)
+
+res_OUwie <- OUwie(phy = treeWIE,
+                   data = dataWIE,
+                   model = "OUM")
+
+save.image(paste0(PATH, "estimation_OUwie", ".RData"))
+rm(vv, treeWIE, dataWIE, res_OUwie)
+
+###############################################################################
+## Summary
+###############################################################################
+load(paste0(PATH, "estimation", name, ".RData"))
+load(paste0(PATH, "estimation_OUwie", ".RData"))
+
+EM <- list(Nbr_shifts = length(results_estim_EM$params$shifts$edges),
+           Nbr_regimes = length(results_estim_EM$params$shifts$edges) + 1,
+           lnL = attr(results_estim_EM$params, "log_likelihood")[1],
+           alpha = results_estim_EM$params$selection.strength,
+           half_life = log(2)/results_estim_EM$params$selection.strength,
+           sigma = results_estim_EM$params$variance)
+
+OU_habitat <- list(Nbr_shifts = Nbr_shifts_OUwie,
+                   Nbr_regimes = 4,
+                   lnL = res_OUwie$loglik,
+                   alpha = res_OUwie$solution["alpha", 1],
+                   half_life = log(2)/res_OUwie$solution["alpha", 1],
+                   sigma = res_OUwie$solution["sigma.sq", 1])
+
+Summary <- cbind(EM, OU_habitat)
+
+write.csv2(Summary, paste0(PATH, "summary", ".RData"))
+save.image(paste0(PATH, "summary", ".RData"))
+           
