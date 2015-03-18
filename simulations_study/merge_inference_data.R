4099e4a4b02796381e3694a6917f4692e8af9aad
pbastide <paul.bastide@gmail.com>
2015-03-18 17:44:08 +0100

    Load if exists

--- simulations_study/merge_inference_data.R
+++ simulations_study/merge_inference_data.R
@@ -34,16 +34,21 @@ source("R/partitionsNumber.R")
 source("R/model_selection.R")
 
 saveresultfile = "../Results/Simulations_Several_K/several_K_estimations"
-datestamp_day <- "2015-03-06"
+datestamp_day <- "2015-03-17"
 
 simestimations_alpha_known <- NULL
 
 for (inference.index in 1:40){
-  load(paste0(saveresultfile, "_alpha_known-", datestamp_day, "_", inference.index, ".RData"))
-  siminf <- as.name(paste0("simestimations_alpha_known_", inference.index))
-  simestimations_alpha_known <- c(simestimations_alpha_known, 
-                                  eval(siminf))
-  rm(list = paste0("simestimations_alpha_known_", inference.index))
+  file <- paste0(saveresultfile, "_alpha_known-", datestamp_day, "_", inference.index, ".RData")
+  if (file.exists(file)) {
+    load(file)
+    siminf <- as.name(paste0("simestimations_alpha_known_", inference.index))
+    simestimations_alpha_known <- c(simestimations_alpha_known, 
+                                    eval(siminf))
+    rm(list = paste0("simestimations_alpha_known_", inference.index))
+  } else {
+    warning(paste0("File number ", inference.index, " do not exists"))
+  }
 }
 
 save.image(paste0(saveresultfile, "_alpha_known-", datestamp_day, "_all", ".RData"))
