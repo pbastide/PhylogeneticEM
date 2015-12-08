319239b4fa90fe3f69f6f3b749fcd41a7ca6bac4
pbastide <paul.bastide@gmail.com>
2015-12-08 13:49:13 -0600

    Ericaceae new version

--- test_cases/ericaceae_migale.R
+++ test_cases/ericaceae_migale.R
@@ -7,6 +7,9 @@ library(combinat) # For alpha prior robust estimation
 library(robustbase) # For robust fitting of alpha
 library(TreeSim)
 
+## Load Ericaceae data
+load("../data/ericaceae_data.RData")
+
 source("R/simulate.R")
 source("R/estimateEM.R")
 source("R/init_EM.R")
@@ -20,77 +23,79 @@ source("R/parsimonyNumber.R")
 source("R/partitionsNumber.R")
 source("R/model_selection.R")
 
-## Load Ericaceae data
-load("../data/ericaceae_data.RData")
-
 datestamp <- format(Sys.time(), "%Y-%m-%d_%H-%M-%S")
 
-## Get a single data frame ################################################
 
-## Select Columns: Corrola lenght (2), anther length (7)
-linear_measures <- data_reduced[, c(1, 2, 7)]
-## Compute species mean (one value per species)
-mean_linear_measures_length <- aggregate(cbind(corolla_tube_length, total_large_anther_length) ~ species,
-                                         data = linear_measures,
-                                         mean,
-                                         na.action = na.pass)
-## Add PC1 and PC2 from Corolla and Anther Shape
-# anther
-# pca_anthers <- data.frame(pca_efou_anthers$x[, 1:2])
-pca_anthers <- data.frame(all_data_pcs_anthers_unique[, 1:3])
-colnames(pca_anthers) <- c("species", "anther.PC1", "anther.PC2")
-# pca_anthers$species <- rownames(pca_anthers)
-# Corolla
-# pca_corolla <- data.frame(pca_efou_flowers$x[, 1:2])
-pca_corolla <- data.frame(all_data_pcs_flowers_unique[, 1:3])
-colnames(pca_corolla) <- c("species", "corolla.PC1", "corolla.PC2")
-# pca_corolla$species <- rownames(pca_corolla)
-# Merge
-dd <- merge(mean_linear_measures_length, pca_anthers, by = "species", all = TRUE) # Match: 4 !!
-traits_data <- merge(dd, pca_corolla, by = "species", all = TRUE)
-## Drop NA (entire ligne is NA)
-traits_data <- traits_data[rowSums(is.na(traits_data)) < (dim(traits_data)[2]-1),]
-rownames(traits_data) <- traits_data[,1]
-percentage_missing <- sum(is.na(traits_data))/prod(dim(traits_data)) * 100
-## Check compatibility with the tree
-Overlap_traits <- name.check(tree, traits_data)
-subtree_traits <- drop.tip(tree, Overlap_traits$Tree.not.data)
-# Sort data in same order as tree
-match_traits <- match(subtree_traits$tip.label, rownames(traits_data))
-sortedData_traits <- traits_data[match_traits, -1]
-trait_matrix <- t(as.matrix(sortedData_traits))
+# ## Get a single data frame ####################################################
+# 
+# ## Select Columns: Corrola lenght (2), anther length (7)
+# linear_measures <- data_reduced[, c(1, 2, 7)]
+# ## Compute species mean (one value per species)
+# mean_linear_measures_length <- aggregate(cbind(corolla_tube_length, total_large_anther_length) ~ species,
+#                                          data = linear_measures,
+#                                          mean,
+#                                          na.action = na.pass)
+# ## Add PC1 and PC2 from Corolla and Anther Shape
+# # anther
+# # pca_anthers <- data.frame(pca_efou_anthers$x[, 1:2])
+# pca_anthers <- data.frame(all_data_pcs_anthers_unique[, 1:3])
+# colnames(pca_anthers) <- c("species", "anther.PC1", "anther.PC2")
+# # pca_anthers$species <- rownames(pca_anthers)
+# # Corolla
+# # pca_corolla <- data.frame(pca_efou_flowers$x[, 1:2])
+# pca_corolla <- data.frame(all_data_pcs_flowers_unique[, 1:3])
+# colnames(pca_corolla) <- c("species", "corolla.PC1", "corolla.PC2")
+# # pca_corolla$species <- rownames(pca_corolla)
+# # Merge
+# dd <- merge(mean_linear_measures_length, pca_anthers, by = "species", all = TRUE) # Match: 4 !!
+# traits_data <- merge(dd, pca_corolla, by = "species", all = TRUE)
+# ## Drop NA (entire ligne is NA)
+# traits_data <- traits_data[rowSums(is.na(traits_data)) < (dim(traits_data)[2]-1),]
+# rownames(traits_data) <- traits_data[,1]
+# percentage_missing <- sum(is.na(traits_data))/prod(dim(traits_data)) * 100
+# ## Check compatibility with the tree
+# Overlap_traits <- name.check(tree, traits_data)
+# subtree_traits <- drop.tip(tree, Overlap_traits$Tree.not.data)
+# # Sort data in same order as tree
+# match_traits <- match(subtree_traits$tip.label, rownames(traits_data))
+# sortedData_traits <- traits_data[match_traits, -1]
+# trait_matrix <- t(as.matrix(sortedData_traits))
 
-## EM inferences - without model selection #######################################
-##################################################################################
-## Fixed quantities
-times_shared <- compute_times_ca(phylo)
-distances_phylo <- compute_dist_phy(phylo)
-subtree.list <- enumerate_tips_under_edges(phylo)
-T_tree <- incidence.matrix(phylo)
-h_tree <- max(diag(as.matrix(times_shared))[1:ntaxa])
 
-## Inference
-set.seed(17920920)
-X <- list(Y_data = Y_data,
-          K_try = 0:K_max,
-          ntaxa = ntaxa)
-## Estimations
-X <- Phylo_EM_sequencial(phylo = subtree_traits,
-                         Y_data = trait_matrix,
-                         process = "BM",
-                         K_max = 10,
-                         curent = X,
-                         random.root = FALSE,
-                         alp = 0,
-                         progress.bar = FALSE,
-                         times_shared = times_shared,
-                         distances_phylo = distances_phylo,
-                         subtree.list = subtree.list,
-                         T_tree = T_tree,
-                         h_tree = h_tree,
-                         save_step = FALSE,
-                         tol = list(variance = 10^(-2), 
-                                    value.root = 10^(-2)),
-                         Nbr_It_Max = 100)
+###############################################################################
+## EM inferences - without model selection ####################################
+###############################################################################
+
+phylo <- subtree_traits[[1]]
+trait_matrix <- trait_matrix_all[[1]]
+
+## Alpha values
+# Re-scale tree
+height_tree <- node.depth.edgelength(tree)[1]
+phylo$edge.length <- tree$edge.length / height_tree
+
+alpha_grid <- find_grid_alpha(phylo,
+                              nbr_alpha = 10,
+                              factor_up_alpha = 2,
+                              factor_down_alpha = 4,
+                              quantile_low_distance = 0.001,
+                              log_transform = TRUE)
+
+## Inference (no model selection)
+# Root fixed
+# Lasso init
+# K_max = 35
+resb <- PhyloEM_core(phylo = phylo,
+                     Y_data = trait_matrix,
+                     process = "scOU",
+                     random.root = FALSE,
+                     K_max = 35,
+                     alpha_known = TRUE,
+                     alpha = alpha_grid,
+                     tol = list(variance = 10^(-2), 
+                                value.root = 10^(-2),
+                                log_likelihood = 10^(-2)),
+                     use_previous = FALSE,
+                     method.init = "lasso")
 
 save.image(file = paste0("../Results/Test_Cases/ericaceae_migale_", datestamp, ".RData"))
