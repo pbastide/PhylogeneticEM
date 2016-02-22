e0788aaeee3c4348c435d919764a4d0ed0ade664
pbastide <paul.bastide@gmail.com>
2016-02-22 17:11:18 +0100

    Exploitation SUN rBM unknown.

--- simulations_study/merge_inference_data.R
+++ simulations_study/merge_inference_data.R
@@ -2,23 +2,26 @@
 ## Bind files together several K
 #############################################
 
-saveresultfile = "../Results/Simulations_Several_K/several_K_estimations_FUN_"
-datestamp_day <- "2015-11-24" # 03-17 11-19
+saveresultfile = "../Results/Simulations_Several_K/several_K_estimations_SUN_rBM"
+datestamp_day <- "2016-02-17" # 03-17 11-19 11-24
 
-simestimations_alpha_known <- NULL
+#simestimations_alpha_known <- NULL
+simestimations <- NULL
+ak <- "" # alpha_known_
 
-for (inference.index in 1:2){
-  file <- paste0(saveresultfile, "alpha_known_", datestamp_day, "_", inference.index, ".RData")
+for (inference.index in 1:4){
+  file <- paste0(saveresultfile, ak, "-", datestamp_day, "_", inference.index, ".RData")
   if (file.exists(file)) {
     load(file)
     ## simestimations
-    siminf <- as.name(paste0("simestimations_alpha_known_", inference.index))
-    simestimations_alpha_known <- c(simestimations_alpha_known, 
+    siminf <- as.name(paste0("simestimations_", ak, inference.index))
+    simestimations <- c(simestimations, 
                                     eval(siminf))
-    rm(list = paste0("simestimations_alpha_known_", inference.index))
+    rm(list = paste0("simestimations_", ak, inference.index))
+    datestamp_day <- "2016-02-17" # 03-17 11-19 11-24
   } else {
     warning(paste0("File number ", inference.index, " do not exists"))
   }
 }
 
-save.image(paste0(saveresultfile, "alpha_known_", datestamp_day, "_all", ".RData"))
+save.image(paste0(saveresultfile, ak, "-", datestamp_day, "_all", ".RData"))
