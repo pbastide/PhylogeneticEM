6bbe97adb31f92c09cff64d9741ce19600c850a0
pbastide <paul.bastide@gmail.com>
2016-05-02 17:31:43 +0200

    Adjust estimation for multivariate

--- simulations_study/multivariate_estimation_SUN_rBM.R
+++ simulations_study/multivariate_estimation_SUN_rBM.R
@@ -16,7 +16,7 @@ datestamp <- format(Sys.time(), "%Y-%m-%d_%H-%M-%S")
 datestamp_day <- format(Sys.time(), "%Y-%m-%d")
 
 ## Load simulated data
-datestamp_data <- "2015-03-17" # 
+datestamp_data <- "2016-04-29" # 
 savedatafile = "../Results/Simulations_Multivariate/multivariate_simlist"
 saveresultfile <- "../Results/Simulations_Multivariate/multivariate_estimations_SUN_rBM"
 load(paste0(savedatafile, "_", datestamp_data, ".RData"))
@@ -35,14 +35,14 @@ source("R/partitionsNumber.R")
 source("R/model_selection.R")
 
 ## These values should be erased by further allocations (generate_inference_files)
-n.range <- n
+n.range <- nrep
 inference.index <- 0
 
-## Select data (according to the value of n)
-n <- n
+## Select data (according to the value of nrep)
+nrep <- nrep
 
 ## Here n.range should be defined by generate_inference_files.R
-simulations2keep <- sapply(simlist, function(x) { x$n %in% n.range }, simplify = TRUE)
+simulations2keep <- sapply(simlist, function(x) { x$nrep %in% n.range }, simplify = TRUE)
 simlist <- simlist[simulations2keep]
 nbrSim <- length(simlist)
 
@@ -76,9 +76,14 @@ estimations_several_K <- function(X){
                  tol = list(variance = 10^(-2), 
                             value.root = 10^(-2),
                             log_likelihood = 10^(-2)),
+                 min_params = list(variance = 0, 
+                                   value.root = -10^(5), 
+                                   exp.root = -10^(5), 
+                                   var.root = 0,
+                                   selection.strength = 0),
                  method.init = "lasso",
                  use_previous = FALSE,
-                 method.selection = "BGH")
+                 method.selection = c("BirgeMassart1", "BirgeMassart2"))
   res <- add_total_time(res)
   # res <- enlight_res(res)
   ret <- list(sim = X,
@@ -102,8 +107,13 @@ add_total_time <- function(res){
   tot_time <- sum(sapply(res[grep("alpha_[[:digit:]]", names(res))],
                          function(z) z$results_summary$time))
   res$alpha_max$results_summary$total_time <- tot_time
-  res$alpha_max$BGH$results_summary$total_time <- tot_time
-  res$alpha_max$BGH$results_summary <- as.data.frame(res$alpha_max$BGH$results_summary)
+  sels <- names(res$alpha_max)[c(grep("DDSE", names(res$alpha_max)),
+                                 grep("Djump", names(res$alpha_max)),
+                                 grep("BGH", names(res$alpha_max)))]
+  for (i in sels){
+    res$alpha_max[[i]]$results_summary$total_time <- tot_time
+    # res$alpha_max[[i]]$results_summary <- as.data.frame(res$alpha_max[i]$results_summary)
+  }
   return(res)
 }
 
@@ -129,7 +139,7 @@ estimations_several_K_ak <- function(X){
                                    selection.strength = 0),
                  method.init = "lasso",
                  use_previous = FALSE,
-                 method.selection = "BGH")
+                 method.selection = c("BirgeMassart1", "BirgeMassart2"))
   res <- add_total_time(res)
   ret <- list(sim = X,
               res = res)
@@ -141,7 +151,7 @@ estimations_several_K_ak <- function(X){
 # ############
 # 
 # ## Separate "favorable" values from others
-# simparams_keep <- subset(simparams, n %in% n.range)
+# simparams_keep <- subset(simparams, nrep %in% n.range)
 # favorables <- simparams_keep$gamma <= 1 & simparams_keep$alpha >= 3 & simparams_keep$K <= 5
 # 
 # ## FAVORABLES ##
@@ -200,54 +210,19 @@ estimations_several_K_ak <- function(X){
 ## Estimations (alpha known)
 ############
 
-## Separate "favorable" values from others
-simparams_keep <- subset(simparams, n %in% n.range)
-favorables <- simparams_keep$gamma <= 1 & simparams_keep$alpha >= 3 & simparams_keep$K <= 5
-
-## FAVORABLES ##
 ## Register parallel backend for computing
 cl <- makeCluster(Ncores)
 registerDoParallel(cl)
 
 ## Parallelized estimations
 time_alpha_known <- system.time(
-  simestimations_fav <- foreach(i = simlist[favorables][1:3], .packages = reqpckg) %dopar%
+  simestimations <- foreach(i = simlist[((1:41) - 1) * 200 + 1], .packages = reqpckg) %dopar%
   {
     estimations_several_K_ak(i)
   }
 )
 # Stop the cluster (parallel)
 stopCluster(cl)
-
-## rename object and save
-assign(paste0("simestimations_fav_alpha_known_", inference.index), 
-       simestimations_fav)
-rm(simestimations_fav)
-
-save.image(paste0(saveresultfile, "favorables_alpha_known_", datestamp_day, "_", inference.index, ".RData"))
-
-## NOT FAVORABLES ##
-## Register parallel backend for computing
-cl <- makeCluster(Ncores)
-registerDoParallel(cl)
-
-## Parallelized estimations
-time_alpha_known_unfav <- system.time(
-  simestimations_unfav <- foreach(i = simlist[!favorables], .packages = reqpckg) %dopar%
-  {
-    estimations_several_K_ak(i)
-  }
-)
-# Stop the cluster (parallel)
-stopCluster(cl)
-
-## group favorables and unfavorables
-simestimations <- vector(mode = "list", length = length(favorables))
-simestimations[favorables] <- eval(as.name(paste0("simestimations_fav_alpha_known_", inference.index)))
-simestimations[!favorables] <- simestimations_unfav
-
-rm(simestimations_unfav)
-rm(list = paste0("simestimations_fav_alpha_known_", inference.index))
 
 ## rename object and save
 assign(paste0("simestimations_alpha_known_", inference.index), 
