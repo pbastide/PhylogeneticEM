80773fe3a8380b02bc1048fb49a818141d7b7685
pbastide <paul.bastide@gmail.com>
2016-05-12 09:45:31 +0200

    exploitation first results.

--- simulations_study/merge_inference_data.R
+++ simulations_study/merge_inference_data.R
@@ -2,14 +2,17 @@
 ## Bind files together several K
 #############################################
 
-saveresultfile = "../Results/Simulations_Several_K/several_K_estimations_SUN_rBM"
-datestamp_day <- "2016-02-17" # 03-17 11-19 11-24
+# saveresultfile = "../Results/Simulations_Several_K/several_K_estimations_SUN_rBM"
+# datestamp_day <- "2016-02-17" # 03-17 11-19 11-24
 
-#simestimations_alpha_known <- NULL
+saveresultfile = "../Results/Simulations_Multivariate/multivariate_estimations_SUN_rBM"
+datestamp_day <- "2016-05-10" 
+
+# simestimations_alpha_known <- NULL
 simestimations <- NULL
-ak <- "" # alpha_known_
+ak <- "alpha_known_"
 
-files_list <- list.files(pattern = "several_K_estimations_SUN_rBM-2016-02-")
+files_list <- list.files(pattern = "multivariate_estimations_SUN_rBM-2016-05-10")
 
 # for (inference.index in 1:50){
 #   file <- paste0(saveresultfile, ak, "-", datestamp_day, "_", inference.index, ".RData")
@@ -21,7 +24,7 @@ for (file in files_list){
     simestimations <- c(simestimations, 
                                     eval(siminf))
     rm(list = paste0("simestimations_", ak, inference.index))
-    datestamp_day <- "2016-02-17" # 03-17 11-19 11-24
+    datestamp_day <- "2016-05-10" # 03-17 11-19 11-24
   } else {
     warning(paste0("File number ", inference.index, " do not exists"))
   }
