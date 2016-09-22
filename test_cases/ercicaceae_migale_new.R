81eed1a526001c4f97c2febd8e9653b86decf487
pbastide <paul.bastide@gmail.com>
2016-09-22 13:42:03 +0200

    New data ericaceae

---
+++ test_cases/ercicaceae_migale_new.R
@@ -0,0 +1,57 @@
+rm(list=ls())
+
+library(doParallel)
+library(foreach)
+library(PhylogeneticEM)
+reqpckg <- c("PhylogeneticEM")
+
+## Load Ericaceae data
+load("../data/ericaceae_data_2016-09-20.RData")
+
+datestamp <- format(Sys.time(), "%Y-%m-%d_%H-%M-%S")
+
+phylo <- comptree_all
+trait_matrix <- t(sortedData_all[,c(2:7)])
+colnames(trait_matrix) <- sortedData_all[,1]
+
+## Alpha values
+# Re-scale tree
+height_tree <- node.depth.edgelength(phylo)[1]
+phylo$edge.length <- phylo$edge.length / height_tree
+
+alpha_grid <- find_grid_alpha(phylo,
+                              nbr_alpha = 10,
+                              factor_up_alpha = 2,
+                              factor_down_alpha = 4,
+                              quantile_low_distance = 0.0005,
+                              log_transform = TRUE)
+
+## Inference (no model selection)
+# Root Stationary
+# Lasso init
+# K_max = 35
+res <- PhyloEM(phylo = phylo,
+               Y_data = trait_matrix,
+               process = "scOU",
+               K_max = 40,
+               random.root = TRUE,
+               stationary.root = TRUE,
+               alpha = alpha_grid[2],
+               save_step = FALSE,
+               Nbr_It_Max = 2000,
+               tol = list(variance = 10^(-2), 
+                          value.root = 10^(-2),
+                          log_likelihood = 10^(-2)),
+               min_params = list(variance = 0, 
+                                 value.root = -10^(5), 
+                                 exp.root = -10^(5), 
+                                 var.root = 0,
+                                 selection.strength = 0),
+               method.variance = "upward_downward",
+               method.init = "lasso",
+               use_previous = FALSE,
+               method.selection = c("BirgeMassart1", "BirgeMassart2"),
+               impute_init_Rphylopars = FALSE,
+               K_lag_init = 5)
+
+save.image(file = paste0("../Results/Test_Cases/ericaceae_all_", datestamp, ".RData"))
